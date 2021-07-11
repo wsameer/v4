a1b0e12922fc9b90b62c34381e8c027b645c4fda
wsameer <dev.wsameer@gmail.com>
2021-07-11 14:16:14 +0530

    fix: integrate the newly created components

--- src/components/SideBox/PersonName/PersonName.tsx
+++ src/components/SideBox/PersonName/PersonName.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const PersonName: React.FC = React.memo(() => {
+export const PersonName: React.FC<{ name: string }> = React.memo(({ name }) => {
     return (
         <h2 className="font-bold title-font uppercase text-white text-2xl tracking-wider">
-            Sameer Waskar
+            {name}
         </h2>
     );
 });
--- src/components/SideBox/SideBox.tsx
+++ src/components/SideBox/SideBox.tsx
@@ -1,45 +1,28 @@
 import Avatar from '@components/common/Avatar';
-import { SocialLink } from '@components/common/SocialLink/SocialLink';
+import Socials from '@components/common/Socials';
 import React from 'react';
-import { GitHub, Twitter, Instagram, Linkedin } from 'react-feather';
+import JobTitle from './JobTitle';
+import PersonName from './PersonName';
 import { IMAGE_SRC } from './constant';
 
+const Divider: React.FC = () => (
+    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+);
+
 export const SideBox: React.FC = () => {
-    const SOCIAL_ACCOUNTS = [
-        {
-            id: 1,
-            logo: <GitHub size={20} />,
-            url: 'https://github.com/wsameer'
-        },
-        {
-            id: 2,
-            logo: <Linkedin size={20} />,
-            url: 'https://www.linkedin.com/in/wsameer'
-        },
-        {
-            id: 3,
-            logo: <Instagram size={20} />,
-            url: 'https://www.instagram.com/the_savage_lens/'
-        },
-        { id: 4, logo: <Twitter size={20} />, url: '@thesavagelens' }
-    ];
     return (
         <div className="md:flex-shrink-0 shadow-lg">
-            <div className="p-10 md:px-4 h-full w-full md:w-80 bg-gray-800">
+            <div className="p-10 md:px-4 h-full w-full md:w-72 lg:w-80 bg-gray-800">
                 <Avatar height={'48'} width={'48'} srcFile={IMAGE_SRC} />
                 <div className="my-4 flex flex-col items-center text-center justify-center">
-                    
-                    <h3 className="font-medium text-gray-400 text-lg m-3">
-                        Software Engineer
-                    </h3>
-                    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                    <div className="flex justify-between">
-                        {SOCIAL_ACCOUNTS.map(({ url, logo }, index) => (
-                            <SocialLink key={index} url={url} logo={logo} />
-                        ))}
-                    </div>
+                    <PersonName name={'Sameer Waskar'} />
+                    <JobTitle value={'Software Engineer'} />
+                    <Divider />
+                    <Socials />
                 </div>
             </div>
         </div>
     );
 };
+
+SideBox.displayName = 'SideBox';
