d67962a66755c8482114f4c25ab9b8965ba4d5a0
wsameer <dev.wsameer@gmail.com>
2021-07-11 22:02:22 +0530

    style: makes section component responsive

--- src/components/layout/Section/Section.tsx
+++ src/components/layout/Section/Section.tsx
@@ -6,7 +6,10 @@ type Props = {
 
 export const Section: React.FC<Props> = React.memo(({ id, children }) => {
     return (
-        <section id={id} className="text-gray-400 bg-gray-800 md:ml-6">
+        <section
+            id={id}
+            className="text-gray-400 bg-gray-800 md:ml-6 md:mb-4 shadow-lg"
+        >
             {children}
         </section>
     );
