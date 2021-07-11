e37fa97d47448d7e16402ce87f282c7532b6b5e6
wsameer <dev.wsameer@gmail.com>
2021-07-11 22:03:52 +0530

    feat: integrates all page sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,45 @@ import Hero from '@components/Hero';
 import PageLayout from '@components/layout/PageLayout';
 import Section from '@components/layout/Section';
 import SideBox from '@components/SideBox';
+import About from '@components/About';
+import Contact from '@components/Contact';
+import Experience from '@components/Experience';
+import Work from '@components/Work';
 
 const App: React.FC = () => {
+    const sections = [
+        {
+            id: 'hero',
+            component: <Hero />
+        },
+        {
+            id: 'about',
+            component: <About />
+        },
+        {
+            id: 'experience',
+            component: <Experience />
+        },
+        {
+            id: 'works',
+            component: <Work />
+        },
+        {
+            id: 'contact',
+            component: <Contact />
+        }
+    ];
     return (
         <div className="min-h-screen bg-gray-700 md:p-6">
             <PageLayout>
                 <SideBox />
-                <Section id="hero">
-                    <Hero />
-                </Section>
+                <div className="flex flex-col">
+                    {sections.map(({ id, component }, key) => (
+                        <Section key={key} id={id}>
+                            {component}
+                        </Section>
+                    ))}
+                </div>
             </PageLayout>
         </div>
     );
