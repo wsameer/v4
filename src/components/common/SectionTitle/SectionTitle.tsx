3598aba0d68aeb1eacd71dd1d5e66e83d4dd893b
wsameer <dev.wsameer@gmail.com>
2021-07-12 19:24:42 +0530

    style: increases font on large devices

--- src/components/common/SectionTitle/SectionTitle.tsx
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const SectionTitle: React.FC = React.memo(() => {
-    return <h2 className="text-3xl md:text-4xl uppercase"></h2>;
+export const SectionTitle: React.FC = React.memo(({ children }) => {
+    return <h2 className="text-2xl md:text-3xl text-white font-bold uppercase mb-6">{children}</h2>;
 });
 
 SectionTitle.displayName = 'SectionTitle';
