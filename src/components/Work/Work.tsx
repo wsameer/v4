dae3be033eb17e4195c0ba35e97add2d1e899f17
wsameer <dev.wsameer@gmail.com>
2021-07-16 19:09:33 +0530

    feat: integrates featuredProject component

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,12 +1,52 @@
+import FeaturedProject from '@components/common/FeaturedProject';
 import SectionTitle from '@components/common/SectionTitle';
 import React from 'react';
+import ProjectDetails from './ProjectDetails';
+import type { ProjectDescProps, ProjectLink } from './ProjectDetails/types';
 
 export const Work: React.FC = () => {
+    const projectOne: ProjectDescProps = {
+            title: 'Octoprofile Plus',
+            description:
+                'A nicer look at your GitHub Profile! With themes, charts and React! Search for your mentors or peers profile, sort through their repos by forks, stars and sizes, toggle the view between list and grid and view some very informative charts.',
+            techList: ['Reactjs', 'GraphQL', 'Github API'],
+            links: [] as Array<ProjectLink>
+        },
+        projectTwo: ProjectDescProps = {
+            title: 'Reakt Dashboard',
+            description:
+                'A simple dashboard app consisting of 3 small React learner projects: Weather app with live timer, Premier League table with live updates and a Todo app with dark skin',
+            techList: ['Reactjs', 'GraphQL', 'Github API'],
+            links: [] as Array<ProjectLink>
+        };
+
     return (
-        <div className="mx-auto flex px-6 md:px-8 py-10 md:py-12 md:flex-row flex-col items-start">
+        <div className="mx-auto px-6 md:px-8 py-10 md:py-12">
             <SectionTitle>
-                <span>My Work</span>
+                <span>Some things I've built</span>
             </SectionTitle>
+            <FeaturedProject
+                leftContent={
+                    <ProjectDetails
+                        title={projectOne.title}
+                        description={projectOne.description}
+                        techList={projectOne.techList}
+                        links={projectOne.links}
+                    />
+                }
+                rightContent={null}
+            />
+            <FeaturedProject
+                leftContent={null}
+                rightContent={
+                    <ProjectDetails
+                        title={projectTwo.title}
+                        description={projectTwo.description}
+                        techList={projectTwo.techList}
+                        links={projectTwo.links}
+                    />
+                }
+            />
         </div>
     );
 };
