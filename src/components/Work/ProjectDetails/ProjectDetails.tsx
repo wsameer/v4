4a548c1a3bf91d998392f40e210157be9d98de8d
wsameer <dev.wsameer@gmail.com>
2021-07-17 12:19:23 +0530

    style: adds custom boxshadow style to project description

--- src/components/Work/ProjectDetails/ProjectDetails.tsx
+++ src/components/Work/ProjectDetails/ProjectDetails.tsx
@@ -8,12 +8,12 @@ export const ProjectDetails: React.FC<ProjectDescProps> = ({
     links
 }) => {
     return (
-        <div className="mb-12">
+        <div className="mb-1">
             <h5 className="mb-1 text-base text-purple-400">Featured project</h5>
             <h3 className="mb-2 text-xl md:text-2xl text-white font-bold">
                 {title}
             </h3>
-            <div className="shadow-xl bg-gray-900 rounded p-4">
+            <div className="shadow-8xl bg-gray-900 rounded p-4">
                 <p className="text-sm md:text-base">{description}</p>
             </div>
             <div className="mt-4 inline-block">
