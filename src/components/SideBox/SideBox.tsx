1a0dd86b9407f62538fb47047a823677fe847580
wsameer <dev.wsameer@gmail.com>
2021-07-25 16:43:49 +0530

    fix(SideBox): lint fixes

--- src/components/SideBox/SideBox.tsx
+++ src/components/SideBox/SideBox.tsx
@@ -5,13 +5,15 @@ import JobTitle from './JobTitle';
 import PersonName from './PersonName';
 import { IMAGE_SRC } from './constant';
 
-const Divider: React.FC = () => <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>;
+const Divider: React.FC = () => (
+    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+);
 
 export const SideBox: React.FC = () => {
     return (
-        <div className="w-full sm:w-72 lg:w-80 md:fixed md:flex-shrink-0">
+        <div className="w-full md:w-72 lg:w-80 md:fixed md:flex-shrink-0">
             <div className="p-10 md:px-4 w-full bg-gray-800 shadow-lg">
-                <Avatar height="48" srcFile={IMAGE_SRC} />
+                <Avatar width={'180'} srcFile={IMAGE_SRC} />
                 <div className="mt-4 flex flex-col items-center text-center justify-center">
                     <PersonName name={'Sameer Waskar'} />
                     <JobTitle value={'Full Stack Developer'} />
