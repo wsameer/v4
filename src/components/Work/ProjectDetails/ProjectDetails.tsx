a5c92c007dddba1210cb69f670728ec08c6bc746
wsameer <dev.wsameer@gmail.com>
2021-07-25 12:07:23 +0530

    style(Work): decreases font size to font-base on all screens

--- src/components/Work/ProjectDetails/ProjectDetails.tsx
+++ src/components/Work/ProjectDetails/ProjectDetails.tsx
@@ -13,14 +13,14 @@ export const ProjectDetails: React.FC<ProjectDescProps> = ({
             <h3 className="text-xl md:text-2xl text-white font-bold">
                 {title}
             </h3>
-            <div className="my-6 p-0 lg:p-4 lg:shadow-8xl bg-transparent lg:bg-gray-900 rounded">
-                <p className="text-base md:text-sm">{description}</p>
+            <div className="my-4 p-0 lg:p-4 lg:shadow-8xl bg-transparent lg:bg-gray-900 rounded">
+                <p className="text-sm">{description}</p>
             </div>
-            <div className="inline-block mb-2">
+            <div className="flex flex-wrap mb-2">
                 {techList?.map((tech, index) => (
                     <span
                         key={index}
-                        className="p-2 mr-2 rounded bg-purple-600 hover:bg-purple-700 text-white text-xs"
+                        className="p-2 m-1 rounded bg-purple-600 hover:bg-purple-700 text-white text-xs"
                     >
                         {tech}
                     </span>
